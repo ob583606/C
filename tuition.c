89193e2c2ee50250207d0023b556a6a1a0aedbdc
ob583606 <50464245+ob583606@users.noreply.github.com>
2021-09-16 13:27:56 -0700

    Assignment 1

---
+++ tuition.c
@@ -0,0 +1,33 @@
+/* tuition.c
+   CSC 111 - Fall 2021 - Assignment 1
+
+   B. Bird - 08/05/2021
+   (Put your name/StudentID/date here when you implement your solution)
+*/
+
+#include <stdio.h>
+
+int main(){
+    /* INPUT DATA */
+    /* You may modify the values of each variable below, but DO NOT rename the variables or move the declarations. */
+
+    double specialized_engineering_units = 1.5;
+    double normal_units = 7.5;
+
+    double cost_per_specialized_engineering_unit = 437.18;
+    double cost_per_normal_unit = 395.08;
+    
+    /* END OF INPUT DATA */
+    /* Implement your solution below this line */
+
+    double total_cost_special_engineering = specialized_engineering_units * cost_per_specialized_engineering_unit;
+    double total_cost_normal_engineering = normal_units * cost_per_specialized_engineering_unit;
+
+    printf("Specialized Engineering units: %.1f, Cost per unit: $%.2f, Total cost: $%.2f\n", specialized_engineering_units, cost_per_normal_unit, total_cost_special_engineering);
+    printf("Normal units: %.1f, Cost per unit: $%.2f, Total cost: $%.2f\n", normal_units, cost_per_normal_unit, total_cost_normal_engineering);
+    printf("Total units: %.1f, Total tuition: $%.2f", normal_units + specialized_engineering_units, total_cost_normal_engineering + total_cost_special_engineering);
+
+    return 0;
+}
+
+
