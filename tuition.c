ea0e93a4f8c57a78d9cd419f7b77f6ef39e16b9e
ob583606 <50464245+ob583606@users.noreply.github.com>
2021-09-16 14:44:29 -0700

    Update tuition.c

--- tuition.c
+++ tuition.c
@@ -20,9 +20,12 @@ int main(){
     /* END OF INPUT DATA */
     /* Implement your solution below this line */
 
+    /* Created two variables that calculate the total cost of either special or regular engineering to make my printf statement that displays the total cost of your entire first
+    year tuition look cleaner. */
     double total_cost_special_engineering = specialized_engineering_units * cost_per_specialized_engineering_unit;
     double total_cost_normal_engineering = normal_units * cost_per_specialized_engineering_unit;
 
+    /* The printf statements that display all of the outputs required. */
     printf("Specialized Engineering units: %.1f, Cost per unit: $%.2f, Total cost: $%.2f\n", specialized_engineering_units, cost_per_normal_unit, total_cost_special_engineering);
     printf("Normal units: %.1f, Cost per unit: $%.2f, Total cost: $%.2f\n", normal_units, cost_per_normal_unit, total_cost_normal_engineering);
     printf("Total units: %.1f, Total tuition: $%.2f", normal_units + specialized_engineering_units, total_cost_normal_engineering + total_cost_special_engineering);
