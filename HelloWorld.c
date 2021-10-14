db4b7f54ed8e5e5914c0db0944abed6e4b1e55ed
ob583606 <50464245+ob583606@users.noreply.github.com>
2021-10-14 15:58:25 -0700

    Hello world is now a testing ground for various functions

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
+void staircase(int n){
+    int x = 0;
+    printf("X");
+    while (x < n) {
+        while(0 < x);
+        printf("-");
+    }
+    printf("x");
+        x -= 1;
+}
+
+int equidistant(int a, int b, int c) {
+    if (a == (b+c)/2 || b == (a+c)/2 || c == (a+b)/2) {
+        return 1;
+    }
+    return 0;
+}
+
 int main() {
 
-    printf("Hello World!");
-
+    equidistant(1, 1, 1);
+    printf("1, 13, 8: %i\n3, 4, 5: %i", equidistant(1, 13, 8), equidistant(3, 4, 5));
     return 0;
     
 }
