5744badd50073192a8d83a46ca1a4ae9893d458f
ob583606 <50464245+ob583606@users.noreply.github.com>
2021-10-27 16:15:46 -0700

    Work on assignment 5

--- HelloWorld.c
+++ HelloWorld.c
@@ -12,6 +12,14 @@ void staircase(int n){
         x -= 1;
 }
 
+void set_vector(int n, double V[n], double s) {
+    int i;
+    for (i = 0; i < n; i++) {
+        V[i] = s;
+    }
+    
+}
+
 int equidistant(int a, int b, int c) {
     if (a == (b+c)/2 || b == (a+c)/2 || c == (a+b)/2) {
         return 1;
@@ -19,10 +27,42 @@ int equidistant(int a, int b, int c) {
     return 0;
 }
 
-int main() {
+void print_vector(int size, double V[size]){
+    for (int i = 0; i < size; i++)
+        printf("%.2f ", V[i]);
+    printf("\n");
+}
 
-    equidistant(1, 1, 1);
-    printf("1, 13, 8: %i\n3, 4, 5: %i", equidistant(1, 13, 8), equidistant(3, 4, 5));
+void add_scalar_to_vector(int n, double Vin[n], double Vout[n], double s) {
+    int i;
+    for(i = 0; i < n; i++) {
+        Vout[i] =  Vin[i] + s;
+    }
+}
+
+double dot_product(int n, double V1[n], double V2[n]) {
+    int i;
+    double total = 0;
+    for(i = 0; i < n; i++){
+        total += V1[i] * V2[i];
+    }
+    return total;
+}
+
+int main() {
+    double V[] = {1, 2, 3, 4, 5};
+    double W[] = {0, 1, 0, 1, 0};
+    double X[] = {10, 20, 30, 3.5, 3.6, 3.1415, 1.7};
+    double Y[] = {111, 116, 225, 230, 265, 370};
+    double Z[] = {0, 0, 0, 999, 999};
+    double Q[100];
+
+    printf("Testing dot_product:\n");
+    printf("Result (V dot W, size 3): %.2f\n", dot_product(3, V, W));
+    printf("Result (V dot W, size 5): %.2f\n", dot_product(5, V, W));
+    printf("Result (W dot X, size 5): %.2f\n", dot_product(5, W, X));
+    printf("\n");
+
     return 0;
     
 }
