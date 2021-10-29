fcf5f81ff6f396d1aa404becfd58068aad283b43
ob583606 <50464245+ob583606@users.noreply.github.com>
2021-10-28 23:11:01 -0700

    Done the damned assignment

--- HelloWorld.c
+++ HelloWorld.c
@@ -34,20 +34,18 @@ void staircase(int n){
         x -= 1;
 }
 
+void print_vector(int size, double V[size]){
+    for (int i = 0; i < size; i++)
+        printf("%.2f ", V[i]);
+    printf("\n");
+}
+
 void set_vector(int n, double V[n], double s) {
     int i;
     for (i = 0; i < n; i++) {
         V[i] = s;
     }
     
-}
-
-
-
-void print_vector(int size, double V[size]){
-    for (int i = 0; i < size; i++)
-        printf("%.2f ", V[i]);
-    printf("\n");
 }
 
 void add_scalar_to_vector(int n, double Vin[n], double Vout[n], double s) {
@@ -235,7 +233,8 @@ void tile(int n, int k, double A[n][k], int s, int t, double B[s][t]) {
 }
 
 int main() {
-    double M1[3][3] = { {1, 2, 3},
+   
+   double M1[3][3] = { {1, 2, 3},
                         {4, 5, 6},
                         {7, 8, 9} };
 
@@ -277,10 +276,96 @@ int main() {
     double V2[] = {9, -9, 1};
     double V3[] = {1, 2, 3, 4};
     
-    double Vout[100];
+    double Vout[100]; //This is deliberately too large so that we can use it as the output for multiple tests.
+        
 
     double I3[3][3], I5[5][5];
+    printf("Testing identity:\n");
+    printf("3 x 3 identity matrix:\n");
+    identity(3, I3);
+    print_matrix(3, 3, I3);
+    printf("\n");
+    printf("5 x 5 identity matrix:\n");
+    identity(5, I5);
+    print_matrix(5, 5, I5);
+    printf("\n");
+    printf("\n");
 
+
+    printf("Testing matrices_equal:\n");
+    printf("Is M1 equal to itself? matrices_equal returns %d\n", matrices_equal(3,3,M1,M1));
+    printf("Are M1 and M2 equal? matrices_equal returns %d\n", matrices_equal(3,3,M1,M2));
+    printf("Are M7 and M8 equal? matrices_equal returns %d\n", matrices_equal(4,3,M7,M8));
+    printf("Are M5 and M7 equal? matrices_equal returns %d\n", matrices_equal(4,3,M5,M7));
+    printf("\n");
+    printf("\n");
+
+    
+    double T1[3][3], T2[5][2];
+    printf("Testing transpose:\n");
+    printf("Transpose of M1:\n");
+    transpose(3, 3, M1, T1);
+    print_matrix(3, 3, T1);
+    printf("\n");
+    printf("Transpose of M3:\n");
+    transpose(2, 5, M3, T2);
+    print_matrix(5, 2, T2); //Note that that the row/column counts are swapped by transposing
+    printf("\n");
+    printf("\n");
+    
+    
+    double OmitRow1[3][4], OmitRow2[4][2];
+    printf("Testing omit_row:\n");
+    printf("Omitting row 0 from M6:\n");
+    omit_row(4, 4, M6, OmitRow1, 0);
+    print_matrix(3, 4, OmitRow1);
+    printf("\n");
+    printf("Omitting row 2 from M6:\n");
+    omit_row(4, 4, M6, OmitRow1, 2);
+    print_matrix(3, 4, OmitRow1);
+    printf("\n");
+    printf("Omitting row 3 from M4:\n");
+    omit_row(5, 2, M4, OmitRow2, 3);
+    print_matrix(4, 2, OmitRow2);
+    printf("\n");
+    
+    
+    double OmitCol1[4][3], OmitCol2[2][4];
+    printf("Testing omit_column:\n");
+    printf("Omitting column 0 from M6:\n");
+    omit_column(4, 4, M6, OmitCol1, 0);
+    print_matrix(4, 3, OmitCol1);
+    printf("\n");
+    printf("Omitting column 3 from M6:\n");
+    omit_column(4, 4, M6, OmitCol1, 3);
+    print_matrix(4, 3, OmitCol1);
+    printf("\n");
+    printf("Omitting column 3 from M3:\n");
+    omit_column(2, 5, M3, OmitCol2, 3);
+    print_matrix(2, 4, OmitCol2);
+    printf("\n");
+    
+    double Add1[3][3], Add2[4][3];
+    printf("Testing add_matrices:\n");
+    printf("M1 + M2:\n");
+    add_matrices(3, 3, M1, M2, Add1);
+    print_matrix(3, 3, Add1);
+    printf("\n");
+    printf("M5 + M7:\n");
+    add_matrices(4, 3, M5, M7, Add2);
+    print_matrix(4, 3, Add2);
+    printf("\n");
+    printf("\n");
+
+
+    printf("Testing trace:\n");
+    printf("Trace of M1: %.2f\n", trace(3, M1));
+    printf("Trace of M2: %.2f\n", trace(3, M2));
+    printf("Trace of M6: %.2f\n", trace(4, M6));
+    printf("\n");
+    printf("\n");
+    
+    
     printf("Testing matrix_vector_multiply:\n");
     printf("(The results of each part are printed as column vectors)\n");
     printf("M1 * V2:\n");
@@ -309,6 +394,52 @@ int main() {
     printf("\n");
     printf("\n");
 
-   return 0;
-    
+
+    double Mul1[3][3], Mul2[2][2], Mul3[5][5], Mul4[4][4], Mul5[4][3];
+    printf("Testing matrix_multiply:\n");
+    printf("M1 * M2:\n");
+    matrix_multiply(3, 3, 3, M1, M2, Mul1);
+    print_matrix(3, 3, Mul1);
+    printf("\n");
+    printf("M3 * M4:\n");
+    matrix_multiply(2, 5, 2, M3, M4, Mul2);
+    print_matrix(2, 2, Mul2);
+    printf("\n");
+    printf("M4 * M3:\n");
+    matrix_multiply(5, 2, 5, M4, M3, Mul3);
+    print_matrix(5, 5, Mul3);
+    printf("\n");
+    printf("M6 * M6:\n");
+    matrix_multiply(4, 4, 4, M6, M6, Mul4);
+    print_matrix(4, 4, Mul4);
+    printf("\n");
+    printf("M6 * M5:\n");
+    matrix_multiply(4, 4, 3, M6, M5, Mul5);
+    print_matrix(4, 3, Mul5);
+    printf("\n");
+    printf("M6 * M7:\n");
+    matrix_multiply(4, 4, 3, M6, M7, Mul5);
+    print_matrix(4, 3, Mul5);
+    printf("\n");
+    printf("\n");
+
+
+    double Tile1[6][6], Tile2[4][5];
+    printf("Testing tile:\n");
+    printf("Tiling M1 to be 6 x 6:\n");
+    tile(3, 3, M1, 6, 6, Tile1);
+    print_matrix(6, 6, Tile1);
+    printf("\n");
+    printf("Tiling M3 to be 6 x 6:\n");
+    tile(2, 5, M3, 6, 6, Tile1);
+    print_matrix(6, 6, Tile1);
+    printf("\n");
+    printf("Tiling M4 to be 6 x 6:\n");
+    tile(5, 2, M4, 6, 6, Tile1);
+    print_matrix(6, 6, Tile1);
+    printf("\n");
+    printf("Tiling M4 to be 4 x 5:\n");
+    tile(5, 2, M4, 4, 5, Tile2);
+    print_matrix(4, 5, Tile2);
+    printf("\n"); 
 }
